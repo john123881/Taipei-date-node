5a8a975e396dcac17da5d0da0752cd70f6459732
John <a123881@gmail.com>
2024-05-14 16:59:02 +0800

    change mysql2

--- utils/mysql2-connect.js
+++ utils/mysql2-connect.js
@@ -1,19 +1,19 @@
-import mysql from "mysql2/promise";
+import mysql from 'mysql2/promise';
 
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
 
 console.log({ DB_HOST, DB_USER, DB_PASS, DB_NAME });
 
 const db = mysql.createPool({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME,
-  // 預設是 3306
-  // port: 3306,
-  waitForConnections: true,
-  connectionLimit: 5,
-  queueLimit: 0,
+    host: '119.14.42.80',
+    user: 'taipei_date',
+    password: '123456',
+    database: 'taipei_date',
+    // 預設是 3306
+    // port: 3306,
+    waitForConnections: true,
+    connectionLimit: 5,
+    queueLimit: 0,
 });
 
 export default db;
